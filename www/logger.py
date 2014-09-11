9d8f2af713ade5eceba4ca7875d7b41c2b86b27e
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2014-09-11 15:22:56 +0200

    Rewrite logger.py
    Cleanup mostly everything, encapsule everyting in a Doorman class
    making it possible to use it as a library.
    
    There are more opportunities for cleanup:
    - optional parameters that are not null in db
    - use python logging instead of homegrown file log and print
    - recycle the cursor
    - split check_user in smaller helpers
    - use fixtures for db data
    
    Still Fablab's profiles are hardcoded which sucks. Fixing that
    requires fixing the db layout which i think is wrong: profiles
    should be stored in the db and there should be only one table
    for the users.

--- www/logger.py
+++ www/logger.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 
-from sys import argv
 import sqlite3
 from datetime import datetime
 import time
+import sys
 import os
-
-source_dir = os.path.dirname(os.path.abspath(__file__))
-cardcode = ''.join(argv[1])
-db = sqlite3.connect(os.path.join(source_dir, 'logger.db'))
-c = db.cursor()
-format = "%Y-%m-%d %H:%M"
 
 class Profile(object):
     def __init__(self, start_time="16:00", stop_time="19:00", weekend=False):
@@ -36,143 +30,134 @@ TIME_PROFILES = {
     "ordinario": Ordinario,
 }
 
-c.execute('''CREATE TABLE IF NOT EXISTS intothedoor(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, datetime TEXT NOT NULL, cardcode TEXT NOT NULL)''')
-c.execute('''CREATE TABLE IF NOT EXISTS fablaballowedusers(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, cardcode TEXT NOT NULL, timeAccessProfile TEXT NOT NULL)''')
-c.execute('''CREATE TABLE IF NOT EXISTS arduinoallowedusers(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, cardcode TEXT NOT NULL)''')
-c.execute('''CREATE TABLE IF NOT EXISTS visitorsallowedusers(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, cardcode TEXT NOT NULL, start_time TEXT NOT NULL, end_time TEXT NOT NULL, expiredate TEXT NOT NULL, startdate TEXT NOT NULL )''')
+class Doorman(object):
+    def __init__(self, db_name=None):
+        if not db_name:
+            db_name = "logger.db"
+        source_dir = os.path.dirname(os.path.abspath(__file__))
+        self.db = sqlite3.connect(os.path.join(source_dir, db_name))
 
-def insert_fabuser(dataBase, username=None, cardcode=None, timeAccessProfile=None):
-  c = db.cursor()
-  log = open('log.txt', 'w')
-  if username and cardcode:
-		c.execute("INSERT INTO fablaballowedusers(username, cardcode, timeAccessProfile) VALUES (?,?,?)", (username, cardcode, timeAccessProfile))
-		dataBase.commit()
-  else:
-		log.write('You need to provide a username and a cardcode')
-		return
-  log.close()
+    def create_db(self):
+        c = self.db.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS intothedoor(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, datetime TEXT NOT NULL, cardcode TEXT NOT NULL)''')
+        c.execute('''CREATE TABLE IF NOT EXISTS fablaballowedusers(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, cardcode TEXT NOT NULL, timeAccessProfile TEXT NOT NULL)''')
+        c.execute('''CREATE TABLE IF NOT EXISTS arduinoallowedusers(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, cardcode TEXT NOT NULL)''')
+        c.execute('''CREATE TABLE IF NOT EXISTS visitorsallowedusers(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, cardcode TEXT NOT NULL, start_time TEXT NOT NULL, end_time TEXT NOT NULL, expiredate TEXT NOT NULL, startdate TEXT NOT NULL )''')
 
-  return
+    def insert_fablab_user(self, username, cardcode, profile=None):
+        """
+        doorman = Doorman()
+        doorman.create_db()
+        doorman.insert_fablab_user(db, 'udirettivo', '1815453204', 'direttivo')
+        doorman.insert_fablab_user(db, 'uhost', '1815453205', 'host')
+        doorman.insert_fablab_user(db, 'uordinario', '1815453206', 'ordinario')
+        """
+        c = self.db.cursor()
+        c.execute("INSERT INTO fablaballowedusers(username, cardcode, timeAccessProfile) VALUES (?,?,?)", (username, cardcode, profile))
+        self.db.commit()
 
-#insert_fabuser(db, 'udirettivo', '1815453204', 'direttivo')
-#insert_fabuser(db, 'uhost', '1815453205', 'host')
-#insert_fabuser(db, 'uordinario', '1815453206', 'ordinario')
+    def insert_arduino_user(self, username, cardcode):
+        """
+        doorman = Doorman()
+        doorman.create_db()
+        doorman.insert_arduino_user(db, 'arduino', '1815453207',)
+        """
+        c = self.db.cursor()
+        c.execute("INSERT INTO arduinoallowedusers(username, cardcode) VALUES (?,?)", (username, cardcode))
+        self.db.commit()
 
-def insert_arduser(dataBase, username=None, cardcode=None):
-  c = db.cursor()
-  log = open('log.txt', 'w')
-  if username and cardcode:
-		c.execute("INSERT INTO arduinoallowedusers(username, cardcode) VALUES (?,?)", (username, cardcode))
-		dataBase.commit()
-  else:
-		log.write('You need to provide a username and a cardcode')
-		return
-  log.close()
+    def insert_visitor_user(self, username, cardcode, start_time=None, end_time=None, expire_date=None, start_date=None):
+        """
+        doorman = Doorman()
+        doorman.create_db()
+        doorman.insert_visitor_user(db, 'visitor', '1815453208', '8', '23', '1404774000','1404720000')
+        """
+        c = self.db.cursor()
+        c.execute("INSERT INTO visitorsallowedusers(username, cardcode, start_time, end_time, expiredate, startdate) VALUES (?,?,?,?,?,?)", (username, cardcode, start_time, end_time, expiredate, startdate))
+        self.db.commit()
 
-  return
+    def log_rfid_read(self, cardcode):
+        date = str(datetime.utcnow().isoformat())
+        c = self.db.cursor()
+        c.execute("INSERT INTO intothedoor(datetime, cardcode) VALUES (?,?)", (date, cardcode))
+        self.db.commit()
 
-#insert_arduser(db, 'arduino', '1815453207',)
+    def check_user(self, cardcode):
+        self.log_rfid_read(cardcode)
 
-def insert_visitorsusers(dataBase, username=None, cardcode=None, start_time=None, end_time=None, expiredate=None, startdate=None ):
-  c = db.cursor()
-  log = open('log.txt', 'w')
-  if username and cardcode:
-		c.execute("INSERT INTO visitorsallowedusers(username, cardcode, start_time, end_time, expiredate, startdate) VALUES (?,?,?,?,?,?)", ( username, cardcode, start_time, end_time, expiredate, startdate))
-		dataBase.commit()
-  else:
-		log.write('You need to provide a username and a cardcode')
-		return
-  log.close()
+        c = self.db.cursor()
+        c.execute("select count (*) from arduinoallowedusers where cardcode=?", (cardcode,))
+        result = c.fetchone()[0]
 
-  return
+        if result > 0:
+            c.execute("select username from arduinoallowedusers where cardcode=?", (cardcode,))
+            name = c.fetchone()[0]
+            print 'y'
+            #print name
+        else:
+            c.execute("select count (*) from visitorsallowedusers where cardcode=?", (cardcode,))
+            result = c.fetchone()[0]
 
-#insert_visitorsusers(db, 'visitor', '1815453208', '8', '23', '1404774000','1404720000')
+            if result > 0:
+                c.execute("select * from visitorsallowedusers where cardcode=?", (cardcode,))
+                nowtime = int(time.time())
+                queryres = c.fetchone()
+                if int(queryres[6]) < nowtime < int(queryres[5]):
+                    now = datetime.today()
+                    nowh = now.strftime('%H')
+                    if int(queryres[3]) <= int(nowh) <= int(queryres[4]):
+                        print 'y'
+                    else:
+                        print 'n'	
+                else:
+                    print 'n' 
+            else:
+                c.execute("select count (*) from fablaballowedusers where cardcode=?", (cardcode,))
+                result = c.fetchone()[0]
 
-def log_rfid_read(dataBase, cardcode):
-  c = db.cursor()
-  date = str(datetime.utcnow().isoformat())
-  c.execute("INSERT INTO intothedoor(datetime, cardcode) VALUES (?,?)", (date, cardcode))
-  dataBase.commit()
-  return
+                if result > 0: # the cardcode is present in the database
+                    # now check the time
+                    c.execute("select timeAccessProfile from fablaballowedusers where cardcode=?", (cardcode,))
+                    timeAccessProfile = c.fetchone()[0] # string corresponding to the timeAccessProfile
+                    klass = TIME_PROFILES.get(timeAccessProfile)
+                    # default profile
+                    if not profile:
+                        klass = Profile
+                    profile = klass()
 
-def check_allowed_user(dataBase, cardcode=None):
-  c = db.cursor()
+                    # some time conversions 
+                    now = datetime.today()
+                    start_date_string = now.strftime("%Y-%m-%d ") + profile.start_time
+                    start_date = datetime.strptime(start_date_string, "%Y-%m-%d %H:%M")
 
-  if cardcode:
-		cardcode = (cardcode,)
-		c.execute("select count (*) from arduinoallowedusers where cardcode=?", cardcode)
-		result = c.fetchone()[0]
-			
-		if result > 0:
-	                        c.execute("select username from arduinoallowedusers where cardcode=?", cardcode)
-				name = c.fetchone()[0]
-				print 'y'
-				#print name
-		
-		else:
-		  
-		     c.execute("select count (*) from visitorsallowedusers where cardcode=?", cardcode)
-		     result = c.fetchone()[0]
-		     
-		     if result > 0:
-				c.execute("select * from visitorsallowedusers where cardcode=?", cardcode)
-				nowtime = int(time.time())
-				queryres = c.fetchone()
-				if  int(queryres[6]) <  nowtime < int(queryres[5]) :
-					                        
-					now = datetime.today()
-					nowh = now.strftime('%H')
-					if int(queryres[3]) <= int(nowh) <= int(queryres[4]):
-						print 'y'
-					else:
-						print 'n'	
-										
-				else:
-					print 'n' 
-		     else:
-				
-			c.execute("select count (*) from fablaballowedusers where cardcode=?", cardcode)
-			result = c.fetchone()[0]
-		
-			if result > 0: # the cardcode is present in the database
-			# now check the time
-			  c.execute("select timeAccessProfile from fablaballowedusers where cardcode=?", cardcode)
-			  timeAccessProfile = c.fetchone()[0] # string corresponding to the timeAccessProfile
-			  klass = TIME_PROFILES.get(timeAccessProfile)
-			  # default profile
-			  if not profile:
-			      klass = Profile
-			  profile = klass()
+                    stop_date_string = now.strftime("%Y-%m-%d ") + profile.stop_time
+                    stop_date = datetime.strptime(stop_date_string, "%Y-%m-%d %H:%M")
 
-			  # some time conversions 
-			  now = datetime.today()
-			  start_date_string = now.strftime("%Y-%m-%d ") + profile.start_time
-			  start_date = datetime.strptime(start_date_string, format)
+                    weekday = now.isoweekday()
+                    c.execute("select username from fablaballowedusers where cardcode=?", (cardcode,))
+                    name = c.fetchone()[0]
+                    if 1 < weekday <= 5 : # weekly day
+                        # check if request is in the time range
+                        if now >= start_date and now <= stop_date:
+                            print 'y'
+                            #print weekday
+                            #print name
 
-			  stop_date_string = now.strftime("%Y-%m-%d ") + profile.stop_time
-			  stop_date = datetime.strptime(stop_date_string, format)
+                        else:
+                            print 'n'
+                            #print 'outofhours'
+                    elif profile.weekend == True:
+                        print 'y'
+                        #print name
+                    else:
+                        print 'n'
+                        #print 'nowend'
 
-			  weekday = now.isoweekday()
-			  c.execute("select username from fablaballowedusers where cardcode=?", cardcode)
-			  name = c.fetchone()[0]
-			  if 1 < weekday <= 5 : # weekly day
-				  # check if request is in the time range
-				if now >= start_date and now <= stop_date:
-					print 'y'
-					#print weekday
-					#print name
-					
-				else:
-					print 'n'
-					#print 'outofhours'
-			  elif profile.weekend == True:
-				  print 'y'
-				  #print name
-			  else:
-				print 'n'
-				#print 'nowend'
-			
-  return
+if __name__ == '__main__':
+    doorman = Doorman()
+    doorman.create_db()
 
-log_rfid_read(db, cardcode)
-check_allowed_user(db, cardcode)
+    if len(sys.argv) != 2:
+        sys.exit(1)
+    cardcode = ''.join(sys.argv[1])
+    doorman.check_user(cardcode)
